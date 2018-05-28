2766c1a069d7fc0f3fbd5dc4c36eaaa1e2b65c19
Mosarvit <artem.moskalew@gmail.com>
2018-05-28 15:09:02 +0200

    m

--- Implementierung/Matlab/JensImplementierung_altered/tests/test_compute_a_from_Uin_Uquest.m
+++ Implementierung/Matlab/JensImplementierung_altered/tests/test_compute_a_from_Uin_Uquest.m
@@ -1,10 +1,14 @@
 function passed = test_compute_a_from_Uin_Uquest()
 
+    addpath(genpath(pwd)); 
+    
     verbosity = 0;
 
-    load('matlab_Workspace_nichtlin_VV_AR.mat');
-    load('..\..\Python\nichtLinear\data\testdata\Uquest_300.csv');
+    load('data\matlab_Workspace_nichtlin_VV_AR.mat');
+    Uquest_300 = load('..\..\Python\nichtLinear\data\testdata\Uquest_300.csv');
 
+%    keyboard;
+    
     a = compute_a_from_Uin_Uquest( U_in(:,2)', 300, Uquest_300(:,2), 3, verbosity );
     
     err = norm(a - a_param2_300) / norm(a_param2_300);
