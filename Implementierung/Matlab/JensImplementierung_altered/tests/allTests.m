fb92b6f9a94663213eef7c73b3e53c91bd090a77
Mosarvit <artem.moskalew@gmail.com>
2018-05-28 07:54:36 -0700

    c

--- Implementierung/Matlab/JensImplementierung_altered/tests/allTests.m
+++ Implementierung/Matlab/JensImplementierung_altered/tests/allTests.m
@@ -9,12 +9,22 @@ function allTests()
             filename = allFiles(i).name;
             lngth = size(filename,2);
             if lngth>3
-                first4 = substr(filename,1,4);
-                if strcmp(first4, 'test')
-                    testFunName = char(substr(filename,1,lngth-2));
+              
+                if is_octave ()
+                  first4 = substr(filename,1,4);
+                else
+                  first4 = extractBetween(filename,1,4);
+                end
+                
+                if strcmp(first4, 'test')                    
+                    if is_octave ()                      
+                      testFunName = char(substr(filename,1,lngth-2));
+                    else                      
+                      testFunName = char(extractBetween(filename,1,lngth-2));
+                    end
                     executeTest(testFunName);
                 end
             end
         end
-    end
+    end  
 end
