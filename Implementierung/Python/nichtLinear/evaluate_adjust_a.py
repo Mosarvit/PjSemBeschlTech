bd789afd48abd1d0d1ebabe3620c12109848aff3
Mosarvit <artem.moskalew@gmail.com>
2018-06-27 01:19:58 +0200

    c

---
+++ Implementierung/Python/nichtLinear/evaluate_adjust_a.py
@@ -0,0 +1,3 @@
+# Mit dieser Routine kann evaluiert werden, ob adjust_a zur Verbessrung des Uout führt.
+# Da die optimierung von a mit der Optimierung von H zuzammenhängt, scheint es sinnvoll, diese Routine erst
+# nach evaluate_adjust_H zu implemnetieren.
