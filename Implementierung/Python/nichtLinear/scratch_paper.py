df54841f6b5f792f3b04b74479e2bbe086897a4f
Mosarvit <artem.moskalew@gmail.com>
2018-08-08 17:29:05 +0200

    c

--- Implementierung/Python/nichtLinear/scratch_paper.py
+++ Implementierung/Python/nichtLinear/scratch_paper.py
@@ -1,2 +1,17 @@
-import nose
-# result = nose.run()
+from unittest import TestCase
+
+from scipy import linalg
+
+from evaluate_adjust_a import evaluate_adjust_a
+from helpers.overlay import overlay
+from helpers.plot_helper import plot_2_signals, plot_2_transfer_functions, plot_3_transfer_functions, plot_3_Ks, plot_K_ideal_Ks, plot_K
+from helpers.signal_helper import calculate_error
+from helpers.csv_helper import save_K, read_in_K
+from settings import mock_system, project_path
+
+data_directory = project_path + 'tests/mock_data/mock_results/adjust_a/'
+K_mock = read_in_K(data_directory + 'K_mock.csv')
+sq= 250
+K_mock = K_mock[sq:997-sq, :]
+save_K(K_mock, data_directory + 'K_mock_trimmed.csv')
+plot_K(K_mock)
--- Implementierung/Python/nichtLinear/tests/test_evaluate_adjust_a.py
+++ Implementierung/Python/nichtLinear/tests/test_evaluate_adjust_a.py
@@ -6,7 +6,8 @@ from evaluate_adjust_a import evaluate_adjust_a
 from helpers.overlay import overlay
 from helpers.plot_helper import plot_2_signals, plot_2_transfer_functions, plot_3_transfer_functions, plot_3_Ks, plot_K_ideal_Ks
 from helpers.signal_helper import calculate_error
-from settings import mock_system
+from helpers.csv_helper import save_K
+from settings import mock_system, project_path
 import colorama
 
 class test_evaluate_adjust_a(TestCase):
@@ -30,20 +31,19 @@ class test_evaluate_adjust_a(TestCase):
 
     def test_evaluate_adjust_a_3_steps(self):
 
-        colorama.init()
+        data_directory = project_path + 'tests/mock_data/mock_results/adjust_a/'
 
-        Uout_ideal, Uout_measured, Ks = evaluate_adjust_a(num_iters=5, verbosity=0)
+        Uout_ideal, Uout_measured, Ks = evaluate_adjust_a(num_iters=1, verbosity=0)
 
-        K_ideal = mock_system.K
+        K_mock = mock_system.K
+        save_K(K_mock, data_directory + 'K_mock.csv')
 
         verbosity = 0
         if verbosity:
-            plot_K_ideal_Ks(K_ideal, Ks)
+            plot_K_ideal_Ks(K_mock, Ks)
 
-
-
-        err_0 = calculate_error(Ks[0], K_ideal)
-        err_last = calculate_error(Ks[-1], K_ideal)
+        err_0 = calculate_error(Ks[0], K_mock)
+        err_last = calculate_error(Ks[-1], K_mock)
 
         self.assertTrue(err_last < err_0)
 
