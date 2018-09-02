c9a2010c7a289908be1f2bb7e4c8e4d55ffeb79f
Mosarvit <artem.moskalew@gmail.com>
2018-09-02 14:37:03 +0800

    с

--- Implementierung/Python/nichtLinear/tests/all_tezts.py
+++ Implementierung/Python/nichtLinear/tests/all_tezts.py
@@ -1,3 +1,6 @@
 import nose
 
+#  all_teyts runs all unit tests.
+# It looks for all filex with the word "test" in it. Therefore the typo in its own name all_tezts is intentional to avoid recursion.
+
 result = nose.run()
